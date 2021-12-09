92cbe007f5191e146339383e07bb43da39bec174
Ahmetcan Aksu <ahmetcanco@gmail.com>
2021-12-09 23:14:43 +0300

    Add salon

--- AddSalon.Designer.cs
+++ AddSalon.Designer.cs
@@ -29,12 +29,79 @@ namespace CinemaLab
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddSalon));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.domainUpDown3 = new System.Windows.Forms.DomainUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(748, 449);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(38)))), ((int)(((byte)(57)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(90, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Ekle";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // domainUpDown3
+            // 
+            this.domainUpDown3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.domainUpDown3.Items.Add("1");
+            this.domainUpDown3.Items.Add("2");
+            this.domainUpDown3.Items.Add("3");
+            this.domainUpDown3.Items.Add("4");
+            this.domainUpDown3.Items.Add("5");
+            this.domainUpDown3.Items.Add("6");
+            this.domainUpDown3.Items.Add("7");
+            this.domainUpDown3.Items.Add("8");
+            this.domainUpDown3.Items.Add("9");
+            this.domainUpDown3.Location = new System.Drawing.Point(90, 256);
+            this.domainUpDown3.Name = "domainUpDown3";
+            this.domainUpDown3.Size = new System.Drawing.Size(300, 19);
+            this.domainUpDown3.TabIndex = 8;
+            this.domainUpDown3.Text = "domainUpDown3";
+            this.domainUpDown3.SelectedItemChanged += new System.EventHandler(this.domainUpDown3_SelectedItemChanged);
+            // 
+            // AddSalon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(748, 447);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.domainUpDown3);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddSalon";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "AddSalon";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DomainUpDown domainUpDown3;
     }
 }
